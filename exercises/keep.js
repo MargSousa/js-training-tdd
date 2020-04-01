0c367475feee681bca7482e96ecd5a52e28f2722
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-04-01 18:35:42 +0100

    keep.js passed

--- exercises/keep.js
+++ exercises/keep.js
@@ -14,8 +14,30 @@
 
 // Your code:
 
+function keepFirst(string) {
+  return string.slice(0, 2);
+};
+
+function keepLast(string) {
+  return string.slice(string.length - 2, string.length );
+};
+
+function keepFirstLast(string) {
+  // return string.substring(2, 4);
+  return string.slice(2, 4);
+};
+
+
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof keepFirst, 'function');
+assert.strictEqual(typeof keepLast, 'function');
+assert.strictEqual(typeof keepFirstLast, 'function');
+assert.strictEqual(keepFirst.length, 1);
+assert.strictEqual(keepLast.length, 1);
+assert.strictEqual(keepFirstLast.length, 1);
+assert.strictEqual(keepFirst('aabbccddeeffgghh'), 'aa');
+assert.strictEqual(keepLast('aabbccddeeffgghh'), 'hh');
+assert.strictEqual(keepFirstLast('aabbccddeeffgghh'), 'bb');
 // End of tests */
