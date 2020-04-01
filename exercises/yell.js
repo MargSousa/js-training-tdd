0f63a881c40bd0d6a433154ab936923e0140572d
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-04-01 17:22:14 +0100

    yell.js passed

--- exercises/yell.js
+++ exercises/yell.js
@@ -8,8 +8,17 @@
 
 // Your code:
 
+function yell(string) {
+  return string.toUpperCase();
+}
+
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof yell, 'function');
+assert.strictEqual(yell.length, 1);
+assert.deepStrictEqual(yell('hello'), 'HELLO');
+assert.deepStrictEqual(yell('AgaIn'), 'AGAIN');
+assert.deepStrictEqual(yell('NOT'), 'NOT');
+
 // End of tests */
